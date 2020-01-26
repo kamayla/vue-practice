68420e9b482f08ece1cefad5ae56986090bfe59e
ippeikamimura <ippei_kamimura@icloud.com>
2020-01-26 21:42:22 +0900

    axios追加
    テストでリストレンダーした。

--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -3,22 +3,31 @@
     <h1 class="title">MyPage</h1>
     <div class="profile-area">
       <p v-on:click="countUp">{{ aaa }}{{ count }}</p>
+      <ul>
+        <li v-for="(text, index) in texts" :key="index">{{ text.title }}</li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'mypage',
   data: function () {
     return {
       aaa: 'aaaa',
-      count: 0
+      count: 0,
+      texts: []
     }
   },
   methods: {
     countUp: function () {
       this.count++
+      axios.get('https://hn.algolia.com/api/v1/search?query=react').then(res => {
+        this.texts = res.data.hits
+        console.log(res.data.hits)
+      })
     }
   }
 }
