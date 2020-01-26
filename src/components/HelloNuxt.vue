fdaea0bda7f4d742b7113739a1f5244be44578a5
ippeikamimura <ippei_kamimura@icloud.com>
2020-01-26 18:10:40 +0900

    first commite

---
+++ src/components/HelloNuxt.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1 class="aaaa">Hello Nuxt!{{ msg }}</h1>
+    <h1 v-on:click="onclickFunction('しこしこしこ')">{{ aaa }}</h1>
+    <input type="text" v-model="aaa">
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      msg: 'aaaaaaaaaaaaa',
+      aaa: 'ははは'
+    }
+  },
+  methods: {
+    onclickFunction: function (text) {
+      this.msg = text
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .aaaa {
+    color: red;
+  }
+</style>
---
+++ src/views/Mypage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mypage">
+    <h1 class="title">MyPage</h1>
+    <div class="profile-area">
+      <p v-on:click="countUp">{{ aaa }}{{ count }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mypage',
+  data: function () {
+    return {
+      aaa: 'aaaa',
+      count: 0
+    }
+  },
+  methods: {
+    countUp: function () {
+      this.count++
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .title {
+    color: pink;
+  }
+  .profile-area {
+    height: 300px;
+    border-radius: 4px;
+    border: 1px solid rgb(243, 242, 242);
+  }
+</style>
